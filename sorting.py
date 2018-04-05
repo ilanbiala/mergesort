56e3250fe3c340d11b456054f751be6f6bb6f8a1
Ilan Biala <ilan.biala@gmail.com>
2018-04-05 16:25:46 -0400

    Fixed a few typos, a small sort test works

--- sorting.py
+++ sorting.py
@@ -1,8 +1,8 @@
 import time, random
 
 def merge(a, start1, start2, end):
-    index1 = start2
-    index2 = start1
+    index1 = start1
+    index2 = start2
     length = end - start1
     aux = [None] * length
     for i in range(length):
@@ -11,8 +11,8 @@ def merge(a, start1, start2, end):
             aux[i] = a[index2]
             index2 += 1
         else:
-            aux[1] = a[index1]
-            index1 += i
+            aux[i] = a[index1]
+            index1 += 1
     for i in range(start1, end):
         a[i] = aux[i - start1]
 
