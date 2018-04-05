c72f402d16225d644ad618efb56c2801e2adae74
Ilan Biala <ilan.biala@gmail.com>
2018-04-05 16:23:20 -0400

    sorting was broken. attempted bug fix. added debugging code.

--- sorting.py
+++ sorting.py
@@ -1,3 +1,5 @@
+import time, random
+
 def merge(a, start1, start2, end):
     index1 = start2
     index2 = start1
@@ -16,7 +18,7 @@ def merge(a, start1, start2, end):
 
 def mergeSort(a):
     n = len(a)
-    step = n
+    step = 1
     while (step < n):
         for start1 in range(0, n, 2*step):
             start2 = min(start1 + step, n)
@@ -28,3 +30,21 @@ arr = [1, 3, 2, 4]
 print(arr)
 mergeSort(arr)
 print(arr)
+
+# builtinSort (wrapped as a function)
+def builtinSort(a):
+    a.sort()
+
+# Generic testSort function
+def testSort(sortFn, n):
+    a = [random.randint(0,2**31) for i in range(n)]
+    sortedA = sorted(a)
+    sortFn(a)
+    assert(a == sortedA)
+
+def testSorts():
+    n = 2**12
+    for sortFn in [mergeSort, builtinSort]:
+        testSort(sortFn, n)
+
+testSorts()
